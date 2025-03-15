32e891692f60f717e846985ef13bbed5c35aba34
Sarvesh Muttepwar <muttepwarsarvesh3958@gmail.com>
2025-03-15 19:44:20 +0530

    Appointment app

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,16 +1,21 @@
 import { Appointments } from './../models/appointments';
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css'],
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem('Appointments');
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : [];
+  }
   newAppointmentTitle: string = '';
   newAppointmentDate: Date = new Date();
-
   appointments: Appointments[] = [];
+
   addAppointment() {
     if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
       let newAppointment: Appointments = {
@@ -21,9 +26,12 @@ export class AppointmentListComponent {
       this.appointments.push(newAppointment);
       this.newAppointmentTitle = '';
       this.newAppointmentDate = new Date();
+
+      localStorage.setItem('Appointments', JSON.stringify(this.appointments));
     }
   }
   deleteAppointment(index: number) {
     this.appointments.splice(index, 1);
+    localStorage.setItem('Appointments', JSON.stringify(this.appointments));
   }
 }
